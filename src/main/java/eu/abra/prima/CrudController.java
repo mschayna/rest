c93a13beed90fbd931a05b5089cbbd89ea787171
Martin Schayna <mschayna@gmail.com>
2012-03-09 00:52:52 +0100

    fixed wrong type parameters

--- src/main/java/eu/abra/prima/CrudController.java
+++ src/main/java/eu/abra/prima/CrudController.java
@@ -1,12 +1,13 @@
 package eu.abra.prima;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.sun.jersey.api.JResponse;
 
-public abstract class CrudController<T extends Serializable> implements ICrudController<T> {
+import eu.abra.prima.beans.CrudObject;
+
+public abstract class CrudController<T extends CrudObject<T>> implements ICrudController<T> {
 
 	protected abstract T newObject();
 	
@@ -14,6 +15,7 @@ public abstract class CrudController<T extends Serializable> implements ICrudCon
 	public JResponse<List<T>> list() {
 		List<T> list = new ArrayList<T>();
 		list.add(newObject());
+		//sd
 		list.add(newObject());
 		list.add(newObject());
 		return JResponse.ok(list).build();
--- src/main/java/eu/abra/prima/beans/Project.java
+++ src/main/java/eu/abra/prima/beans/Project.java
@@ -3,6 +3,6 @@ package eu.abra.prima.beans;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "project")
-public class Project extends CrudObject<TimeRecord> {
+public class Project extends CrudObject<Project> {
 	private static final long serialVersionUID = 1L;
 }
